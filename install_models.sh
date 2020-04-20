519d86232515c72fe87fc70a7c54aa9fbd54c143
Andrew Gibiansky <andrew.gibiansky@gmail.com>
2020-04-20 23:22:25 +0000

    add --no-check-certificates to wget

--- install_models.sh
+++ install_models.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 VERSION="0.03"
 
@@ -8,7 +8,7 @@ download_models() {
 	local version="$1"
 	local filename="kaldi-models-$version.zip"
 	local url="https://lowerquality.com/gentle/$filename"
-	wget -O $filename $url
+	wget -O $filename $url --no-check-certificate
 	unzip $filename
 	rm $filename
 }
